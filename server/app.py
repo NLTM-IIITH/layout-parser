4a0c6cd38b2dfd4b3b8c69ef1c9d6fb92b4e7dd4
Nishanth artham <arthamnishanth123@gmail.com>
2022-06-01 15:05:26 +0530

    Refactored the API

--- server/app.py
+++ server/app.py
@@ -23,6 +23,56 @@ async def doctr_layout_parser(image: UploadFile = File(...)):
 	regions = process_image(image_path)
 	return LayoutResponse(regions=regions)
 
+# @app.post('/preprocess/binarize', tags=['Pre Process'], response_model=PreProcessorResponse)
+# async def binarize_image(image: UploadFile = File(...)):
+# 	"""
+# 	Returns the binarized image
+# 	"""
+# 	image_path = save_uploaded_image(image)
+# 	regions = process_image(image_path)
+# 	return PreProcessorResponse(images=regions)
+@app.post('/preprocess/binarize', tags=['Pre Process'], response_model=PreProcessorBinarizeResponse)
+async def binarize_image(images: List[bytes] = File(...)):
+	"""
+	Returns the binarized image
+	"""
+	image_path = save_uploaded_image(images)
+	regions = process_image(image_path)
+	return PreProcessorBinarizeResponse(images=regions)
+@app.post('/preprocess/grayscale', tags=['Pre Process'], response_model=PreProcessorGrayScaleResponse)
+
+async def grayscale_image(images: List[bytes] = File(...)):
+	"""
+	Returns the Grayscale image
+	"""
+	image_path = save_uploaded_image(images)
+	regions = process_image(image_path)
+	return PreProcessorGrayScaleResponse(images=regions)
+@app.post('/preprocess/color', tags=['Pre Process'], response_model=PreProcessorColorResponse)
+async def Get_Image_Colors(images: List[bytes] = File(...)):
+	"""
+	Returns the binarized image
+	"""
+	image_path = save_uploaded_image(images)
+	regions = process_image(image_path)
+	return PreProcessorColorResponse(images=regions)
+@app.post('/preprocess/font', tags=['Pre Process'], response_model=PreProcessorFontResponse)
+async def Get_Font_Properties_in_the_Image(images: List[bytes] = File(...)):
+	"""
+	Returns the binarized image
+	"""
+	image_path = save_uploaded_image(images)
+	regions = process_image(image_path)
+	return PreProcessorFontResponse(images=regions)
+@app.post('/preprocess/properties', tags=['Pre Process'], response_model=PreProcessorPropertiesResponse)
+async def Get_Image_Properties(images: List[bytes] = File(...)):
+	"""
+	Returns the binarized image
+	"""
+	image_path = save_uploaded_image(images)
+	regions = process_image(image_path)
+	return PreProcessorPropertiesResponse(images=regions)
+
 
 @app.post('/layout/demo', tags=['Demo'])
 async def layout_parser_swagger_only_demo(
