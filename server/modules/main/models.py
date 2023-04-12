NLTM-IIITH/layout-parser

commit 8c922f4115fcbdb2ef8b95622ab64d2c6c63353e

added option for dilation of words

server/modules/main/models.py
@@ -25,6 +25,15 @@ class BoundingBox(BaseModel):
 		description='height of the bbox (in pixel)'
 	)
 
+	@classmethod
+	def from_xyxy(cls, coords: tuple[int, int, int, int]) -> 'BoundingBox':
+		return cls(
+			x=coords[0],
+			y=coords[1],
+			w=coords[2] - coords[0],
+			h=coords[3] - coords[1]
+		)
+
 
 class Region(BaseModel):
 	bounding_box: BoundingBox
@@ -33,6 +42,22 @@ class Region(BaseModel):
 		0,
 		description='Stores the sequential line number of the para text starting from 1'
 	)
+
+	def to_xyxy(self) -> tuple[int, int, int, int]:
+		return (
+			self.bounding_box.x,
+			self.bounding_box.y,
+			self.bounding_box.x + self.bounding_box.w,
+			self.bounding_box.y + self.bounding_box.h
+		)
+
+	@classmethod
+	def from_xyxy(cls, coords: tuple[int, int, int, int], label='', line=0):
+		return cls.from_bounding_box(
+			bbox=BoundingBox.from_xyxy(coords),
+			label=label,
+			line=line
+		)
 
 	@classmethod
 	def from_bounding_box(cls, bbox, label='', line=0):

server/modules/main/post_helper.py
@@ -1,0 +1,55 @@
+from os.path import join
+
+import cv2
+from tqdm import tqdm
+
+from ..core.config import IMAGE_FOLDER
+from .models import LayoutImageResponse, Region
+
+
+def process_region(img, region: Region) -> Region:
+	line = region.line
+	label = region.label
+	x1, y1, x2, y2 = region.to_xyxy()
+
+	# process left
+	while img[y1:y2, x1].sum() > 0:
+		x1 -= 1
+	
+	# process top
+	while img[y1, x1:x2].sum() > 0:
+		y1 -= 1
+	
+	# process right
+	while img[y1:y2, x2].sum() > 0:
+		x2 += 1
+	
+	# process bottom
+	while img[y2, x1:x2].sum() > 0:
+		y2 += 1
+
+	return Region.from_xyxy(
+		(x1, y1, x2, y2),
+		line=line,
+		label=label
+	)
+
+
+def process_dilate(regions: list[Region], image_path: str) -> list[Region]:
+	img = cv2.imread(image_path, 0)
+	_, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
+	ret = []
+	for region in tqdm(regions):
+		ret.append(process_region(img, region))
+	return ret
+
+def process_multiple_dilate(inp: list[LayoutImageResponse]) -> list[LayoutImageResponse]:
+	ret = []
+	for i in inp:
+		ret.append(
+			LayoutImageResponse(
+				image_name=i.image_name,
+				regions=process_dilate(i.regions, join(IMAGE_FOLDER, i.image_name))
+			)
+		)
+	return ret

server/modules/main/routes.py
@@ -12,6 +12,7 @@ from .helper import (process_image, process_image_craft,
                      process_multiple_image_doctr_v2,
                      process_multiple_image_worddetector, save_uploaded_image)
 from .models import LayoutImageResponse, ModelChoice
+from .post_helper import process_dilate, process_multiple_dilate
 
 router = APIRouter(
 	prefix='/layout',
@@ -22,26 +23,31 @@ router = APIRouter(
 @router.post('/', response_model=List[LayoutImageResponse])
 async def doctr_layout_parser(
 	folder_path: str = Depends(save_uploaded_images),
-	model: ModelChoice = Form(ModelChoice.doctr)
+	model: ModelChoice = Form(ModelChoice.doctr),
+	dilate: bool = Form(False),
 ):
 	"""
 	API endpoint for calling the layout parser
 	"""
 	print(model.value)
 	if model == ModelChoice.craft:
-		return process_multiple_image_craft(folder_path)
+		ret = process_multiple_image_craft(folder_path)
 	elif model == ModelChoice.worddetector:
-		return process_multiple_image_worddetector(folder_path)
+		ret = process_multiple_image_worddetector(folder_path)
 	elif model == ModelChoice.doctr:
-		return process_multiple_image_doctr(folder_path)
+		ret = process_multiple_image_doctr(folder_path)
 	elif model == ModelChoice.v2_doctr:
-		return process_multiple_image_doctr_v2(folder_path)
+		ret = process_multiple_image_doctr_v2(folder_path)
+	if dilate:
+		ret = process_multiple_dilate(ret)
+	return ret
 
 
 @router.post('/visualize')
 async def layout_parser_swagger_only_demo(
 	image: UploadFile = File(...),
-	model: ModelChoice = Form(ModelChoice.doctr)
+	model: ModelChoice = Form(ModelChoice.doctr),
+	dilate: bool = Form(False),
 ):
 	"""
 	This endpoint is only used to demonstration purposes.
@@ -57,6 +63,8 @@ async def layout_parser_swagger_only_demo(
 		regions = process_image_worddetector(image_path)
 	else:
 		regions = process_image(image_path, model.value)
+	if dilate:
+		regions = process_dilate(regions, image_path)
 	save_location = '/home/layout/layout-parser/images/{}.jpg'.format(
 		str(uuid.uuid4())
 	)

commit 0178813d69426481e14564371971b75c737b707f

layout-parser: Fixing 'type' object is not subscriptable error
Fixing the below error due to incorrect object type usage

server/modules/main/models.py", line 30, in BoundingBox
    def from_xyxy(cls, coords: tuple[int, int, int, int]) -> 'BoundingBox':
TypeError: 'type' object is not subscriptable

Fixes: 8c922f4115fcb ("added option for dilation of words")
Signed-off-by: Devesh Sharma <sharma.98@iitj.ac.in>

server/modules/main/models.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import List, Optional
+from typing import List, Tuple, Optional
 
 from pydantic import BaseModel, Field
 
@@ -26,7 +26,7 @@ class BoundingBox(BaseModel):
 	)
 
 	@classmethod
-	def from_xyxy(cls, coords: tuple[int, int, int, int]) -> 'BoundingBox':
+	def from_xyxy(cls, coords: Tuple[int, int, int, int]) -> 'BoundingBox':
 		return cls(
 			x=coords[0],
 			y=coords[1],
@@ -43,7 +43,7 @@ class Region(BaseModel):
 		description='Stores the sequential line number of the para text starting from 1'
 	)
 
-	def to_xyxy(self) -> tuple[int, int, int, int]:
+	def to_xyxy(self) -> Tuple[int, int, int, int]:
 		return (
 			self.bounding_box.x,
 			self.bounding_box.y,
@@ -52,7 +52,7 @@ class Region(BaseModel):
 		)
 
 	@classmethod
-	def from_xyxy(cls, coords: tuple[int, int, int, int], label='', line=0):
+	def from_xyxy(cls, coords: Tuple[int, int, int, int], label='', line=0):
 		return cls.from_bounding_box(
 			bbox=BoundingBox.from_xyxy(coords),
 			label=label,