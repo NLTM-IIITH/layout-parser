b482ae8a4b4e8f841bc314f40230817b87c3e310
kuna71 <kankariakunal71@gmail.com>
2023-11-07 18:53:13 +0530

    Added MOdelChoice for script-identification

--- server/modules/postprocess/models.py
+++ server/modules/postprocess/models.py
@@ -1,7 +1,10 @@
 from pydantic import BaseModel, Field
-
+from enum import Enum
 # SI stands for Script Identification
 
+class ModelChoice(str, Enum):
+    alexnet = 'alexnet'
+    default = 'default'	#Temporarily calling it default due to lack of knowledge regarding nature of model
 class PostprocessRequest(BaseModel):
 	images: list[str] = Field(
 		...,
