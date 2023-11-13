894207b1ac62c80c65aa151a5cc09e2cc18408fe
shouryatyagi222 <shouryatyagi222@gmail.com>
2023-11-13 09:25:59 +0530

    Adding Routes for Textron and Doctr

--- server/app.py
+++ server/app.py
@@ -4,13 +4,15 @@ from dateutil.tz import gettz
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
+from .modules.textron_api.routes import router as textron_router
+from .modules.doctr_api.routes import router as doctr_router
 from .modules.cegis.routes import router as cegis_router
 from .modules.main.routes import router as main_router
 from .modules.postprocess.routes import router as postprocess_router
 from .modules.preprocess.routes import router as preprocess_router
 
 app = FastAPI(
-	title='Layout Parser API',
+	title='Layout API',
 	description='',
 	docs_url='/layout/docs',
 	openapi_url='/layout/openapi.json'
@@ -30,6 +32,8 @@ app.add_middleware(
 	allow_credentials=True,
 )
 
+app.include_router(textron_router)
+app.include_router(doctr_router)
 app.include_router(preprocess_router)
 app.include_router(main_router)
 app.include_router(cegis_router)
--- server/modules/core/config.py
+++ server/modules/core/config.py
@@ -1,3 +1,13 @@
+### Path to the Data and Results directories
 
-# this if the folder where all the input images are saved.
-IMAGE_FOLDER = '/home/layout/layout-parser/images'
+# NOTE : ALL THE PATHS ARE RELATIVE TO THE LAYOUT-PARSER-API
+
+INPUT_DATA_DIR = ''          # input dir where all the input files from the request are saved 
+RESULTS_DATA_DIR  = 'server/modules/textron_results'   # output Dir where textron results are to be saved
+
+# this is the folder where all the input images are saved.
+IMAGE_FOLDER = 'images'    # input dir in INPUT_DATA_DIR where images are saved
+PRED_IMAGES_FOLDER='server/modules/textron_results/predictions/devanagari'    # output predicted images from textron 
+PRED_TXT_FOLDER='server/modules/textron_results/txt/devanagari'   # output txt files containing the bboxes from textron
+PRED_CAGE_FOLDER='server/modules/textron_results/cage/devanagari'  # output cage files 
+TEXTRON_MAIN_FILE='server.modules.textron_api.main'       # a path to the main.py of the textron to be run - in this format because other this were giving relative import error
