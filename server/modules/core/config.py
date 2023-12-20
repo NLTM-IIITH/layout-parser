318118541d6f0880d8d8f65a5cd193a130cb0c13
shouryatyagi222 <shouryatyagi222@gmail.com>
2023-12-20 17:44:17 +0530

    Update for Docker-textron

--- server/modules/core/config.py
+++ server/modules/core/config.py
@@ -1,13 +1,3 @@
-### Path to the Data and Results directories
-# NOTE : ALL THE PATHS ARE RELATIVE TO THE LAYOUT-PARSER-API
-# this is the folder where all the input images are saved.
-IMAGE_FOLDER = 'images'
 
-#  Textron folders
-INPUT_DATA_DIR = ''          # input dir where all the input files from the request are saved 
-RESULTS_DATA_DIR  = 'server/modules/textron/textron_results'   # output Dir where textron results are to be saved
-PRED_IMAGES_FOLDER='server/modules/textron/textron_results/predictions/devanagari'    # output predicted images from textron 
-PRED_TXT_FOLDER='server/modules/textron/textron_results/txt/devanagari'   # output txt files containing the bboxes from textron
-PRED_CAGE_FOLDER='server/modules/textron/textron_results/cage/devanagari'  # output cage files 
-TEXTRON_MAIN_FILE='server.modules.textron.main'       # a path to the main.py of the textron to be run - in this format because other this were giving relative import error
-TEXTRON_DEPENDENCIES='server/modules/textron/textron_results/textron_dependencies/'
+# this if the folder where all the input images are saved.
+IMAGE_FOLDER = '/media/shourya/B018CE3C18CE0178/IITB_OCR/layout-parser/layout-parser-api/images'
