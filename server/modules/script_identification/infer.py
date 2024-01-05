2ce1f85e0da473617097245a092c499a9cd067a3
kuna71 <kankariakunal71@gmail.com>
2024-01-05 11:47:34 +0530

    removed unnecessary files

--- server/modules/script_identification/infer.py
+++
@@ -1,9 +0,0 @@
-import json
-import os
-
-from helper import *
-from models import *
-
-print(os.listdir("data"))
-#Perform inference and save output in output.json
-script_inference_alexnet("data")
