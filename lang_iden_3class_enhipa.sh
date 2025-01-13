f42e6200d52d2d5cc0d56ad979a530ca3b9802d7
Krishna Tulsyan <kt.krishna.tulsyan@gmail.com>
2025-01-13 14:53:48 +0530

    removed unnessacary prints for pola script detection

--- lang_iden_3class_enhipa.sh
+++ lang_iden_3class_enhipa.sh
@@ -4,21 +4,15 @@ DATA_DIR="$1"
 
 MODEL_DIR="/home/layout/models/language_identification_3class_enhipa"
 
-echo "Checking for data dir"
 if [ ! -d "$DATA_DIR" ]; then
 	echo "$DATA_DIR : Enter a valid data directory"
 	exit
-else
-	echo -e "DATA_DIR\t$DATA_DIR"
 fi
 
 
-echo "Checking for model dir"
 if [ ! -d "$MODEL_DIR" ]; then
 	echo "$MODEL_DIR : Enter a valid model directory"
 	exit
-else
-	echo -e "MODEL_DIR\t$MODEL_DIR"
 fi
 
 docker run --rm --gpus all --net host \
